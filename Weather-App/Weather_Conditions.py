257e737061e31c5887af26395c55041a1593e9df
mr-Ucar <116120748+mr-Ucar@users.noreply.github.com>
2023-12-15 23:59:10 +0300

    Update Weather_Conditions.py

--- Weather-App/Weather_Conditions.py
+++ Weather-App/Weather_Conditions.py
@@ -5,25 +5,27 @@ import termcolor
 import re
 from time import sleep
 
-print(termcolor.colored("╰☆☆ ☀ ☁ ☂ ☼ ❄ ☾ ℉  Weather Conditions App by Mr-Ucar ☆☆╮", "green"))
+print(termcolor.colored("╰☆☆ ☀ ☁ ☂ ☼ ❄ ☾ ℉  Weather App by Mr-Ucar ☆☆╮", "green"))
 sleep (0.1)
-print(termcolor.colored("\t☮ Welcome to the simple Weather Conditions App ☮", "light_blue"))
+print(termcolor.colored("\t☮ Welcome to the simple Weather App ☮", "light_blue"))
 sleep (0.1)
 print(termcolor.colored("\n\tTurkish characters can cause a problem...Some citynames like 'Aydın' should be written as 'Aydin'", "magenta"))
 sleep (0.2)
 
-
-# While I was testing the program, I noticed that the program was not working properly when I entered the city name with Turkish characters.
-# it was easy to fix it by 'replace' function in Python, but then cities like Lisbon, or Aydın were not working properly at the same time.
-#In order to fix it again, I needed to write -if, elif, else statements- for each city name. And it made the code unnecessarily long.
-#So I decided to use 're' module to check if the city name contains any Turkish characters or numbers and also good to see if the user provides strings only. Better not to let some payloads to be given as user input:)
+attempt = 0
+max_attempts = 2
 
 city = input("\t\nWhich city to check?:  \n\t\n")
-while not re.match("^[a-zA-Z\s]+$", city):
-    print("Invalid city name. Please enter a valid city name without numbers, Turkish characters or weird characters like ?\-_)=}[], etc...") 
+while (not re.match("^[a-zA-Z\sğüşıöçĞÜŞİÖÇ]+$", city) or re.search("[^\x00-\x7F]", city)) and attempt < max_attempts: #"[^\x00-\x7F]" matches any non-ASCII character
+    print("Invalid city name. Please enter a valid city name without numbers, Turkish characters or weird characters like ?'/, etc...\n") 
     city = city.strip()
     city = city.title()
     city = city.replace(" ", "")
+    attempt += 1
+    print(f"Invalid Character #{attempt}")
+
+if attempt == max_attempts:
+    print("\n\tCharacter error. Correcting...\n")
 
 else:
     print(termcolor.colored(f"\n\tYou entered {city}.\n", "green"))
@@ -35,7 +37,7 @@ def get_current_weather(city):
     """Retrieves the current weather for the specified city from OpenWeatherMap.""" 
     print(termcolor.colored(f"Retrieving weather information for {city}...", "cyan"))
     sleep (0.1)
-    api_key = "Write your API here" # For security, I deleted my API key, you can get yours free of charge by signing up in http://api.openweathermap.org
+    api_key = "WRITE YOUR OWN API" #You can get your API free of charge from openweathermap.org
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
     
     response = requests.get(url)
@@ -47,12 +49,13 @@ def get_current_weather(city):
         weather = data["weather"][0]["description"]
         sleep (0.1)
         temperature = data["main"]["temp"]
+        temp_k = data["main"]["temp"]
+        temp_c = temp_k - 273.15
+        print(f"Temperature: {temp_k} K ({temp_c} C)")
         sleep (0.1)
         humidity = data["main"]["humidity"]
         
         print(f"Weather in {city}: {weather}")
-        sleep (0.1)
-        print(f"Temperature: {temperature} K")
         sleep (0.1)
         print(f"Humidity: {humidity}%")
         sleep (0.1)
